f8a9738a8dd327a5bc99bfe7e69055e67e495af8
asdfvar <asdfvar@gmail.com>
2017-10-22 19:38:29 -0700

    inter-group communication

--- mpi/groups_3/group0.c
+++ mpi/groups_3/group0.c
@@ -6,54 +6,62 @@ int main(int argc, char **argv)
 { 
    MPI_Comm   myComm;       /* intra-communicator of local sub-group */ 
    MPI_Comm   myFirstComm;  /* inter-communicator */ 
-   MPI_Comm   mySecondComm; /* second inter-communicator (group 1 only) */ 
    int group_number = 0; 
-   int rank; 
+   int global_rank; 
+   int local_rank; 
    MPI_Status status;
 
 
-   MPI_Init(&argc, &argv); 
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-printf("%s:%d:rank %d\n", __FILE__, __LINE__, rank);
+   MPI_Init( &argc, &argv );
+   MPI_Comm_rank( MPI_COMM_WORLD, &global_rank );
 
    /* Build intra-communicator for local sub-group */ 
    MPI_Comm_split( MPI_COMM_WORLD, // comm
                    group_number,   // color
-                   rank,           // key
+                   global_rank,    // key
                   &myComm );       // newcomm
 
+   MPI_Comm_rank( myComm, &local_rank );
+printf("%s:%d:global_rank %d:local_rank %d\n", __FILE__, __LINE__, global_rank, local_rank);
+
    /* Build inter-communicators.  Tags are hard-coded. */ 
-   /* Group 0 communicates with group 1 and 2 */
+   /* Group 0 communicates with group 1 */
 
    MPI_Intercomm_create( myComm,         // local_comm
                          0,              // local_leader
                          MPI_COMM_WORLD, // peer_comm
-                         1,              // remote_leader
+                         2+local_rank,   // remote_leader
                          1,              // tag
                          &myFirstComm ); // newintercomm
 
-   MPI_Intercomm_create( myComm,          // local_comm
-                         0,               // local_leader
-                         MPI_COMM_WORLD,  // peer_comm
-                         2,               // remote_leader
-                         12,              // tag
-                         &mySecondComm ); // newintercomm
-
    /* Do work ... */ 
    float numbers[3];
-   MPI_Recv( numbers,     // buf
-             3,           // count
-             MPI_FLOAT,   // datatype
-             0,           // source (rank of source)
-             TAG_0,       // tag
-             myFirstComm, // comm
-            &status );    // status
 
-   printf("numbers = %f, %f, %f\n", numbers[0], numbers[1], numbers[2]);
+   if (local_rank == 0)
+   {
+      MPI_Recv( numbers,     // buf
+                3,           // count
+                MPI_FLOAT,   // datatype
+                0,           // source (rank of source)
+                TAG_0,       // tag
+                myFirstComm, // comm
+               &status );    // status
+   }
+   if (local_rank == 1)
+   {
+      MPI_Recv( numbers,     // buf
+                3,           // count
+                MPI_FLOAT,   // datatype
+                1,           // source (rank of source)
+                TAG_3,       // tag
+                myFirstComm, // comm
+               &status );    // status
+   }
+
+   printf("local rank %d numbers = %f, %f, %f\n", local_rank, numbers[0], numbers[1], numbers[2]);
 
    // close down stuff
    MPI_Comm_free(&myFirstComm); 
-   MPI_Comm_free(&mySecondComm); 
    MPI_Finalize(); 
 
    return 0;
--- mpi/groups_3/group1.c
+++ mpi/groups_3/group1.c
@@ -7,27 +7,55 @@ int main(int argc, char **argv)
    MPI_Comm   myComm;       /* intra-communicator of local sub-group */ 
    MPI_Comm   myFirstComm;  /* inter-communicator */ 
    int group_number = 1; 
-   int rank; 
+   int global_rank; 
+   int local_rank; 
 
 
    MPI_Init(&argc, &argv); 
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
-printf("%s:%d:rank %d\n", __FILE__, __LINE__, rank);
+   MPI_Comm_rank( MPI_COMM_WORLD, &global_rank );
 
    /* Build intra-communicator for local sub-group */ 
-   MPI_Comm_split(MPI_COMM_WORLD, group_number, rank, &myComm); 
+   MPI_Comm_split( MPI_COMM_WORLD, // comm
+                   group_number,   // color
+                   global_rank,    // key
+                  &myComm );       // newcomm
 
+   MPI_Comm_rank( myComm, &local_rank );
+printf("%s:%d:global_rank %d:local_rank %d\n", __FILE__, __LINE__, global_rank, local_rank);
 
    /* Build inter-communicators.  Tags are hard-coded. */ 
    /* Group 1 communicates with group 0 */ 
-   MPI_Intercomm_create( myComm, 0, MPI_COMM_WORLD, 0, 1, &myFirstComm); 
 
+   MPI_Intercomm_create( myComm,         // local_comm
+                         0,              // local_leader
+                         MPI_COMM_WORLD, // peer_comm
+                         local_rank,     // remote_leader
+                         1,              // tag
+                        &myFirstComm );  // newintercomm
 
    /* Do work ... */ 
    float numbers[3] = {10, 12, 13};
-   MPI_Send( numbers, 3, MPI_FLOAT, 0, TAG_0, myFirstComm );
 
-   printf("%s: hello\n", __FILE__);
+   if (local_rank == 0)
+   {
+      numbers[0] = 0;
+      MPI_Send( numbers,       // buf
+                3,             // count
+                MPI_FLOAT,     // datatype
+                0,             // rank of destination
+                TAG_0,         // tag
+                myFirstComm ); // comm
+   }
+   if (local_rank == 1)
+   {
+      numbers[0] = 1;
+      MPI_Send( numbers,       // buf
+                3,             // count
+                MPI_FLOAT,     // datatype
+                1,             // rank of destination
+                TAG_3,         // tag
+                myFirstComm ); // comm
+   }
 
    // close down stuff
    MPI_Comm_free(&myFirstComm); 

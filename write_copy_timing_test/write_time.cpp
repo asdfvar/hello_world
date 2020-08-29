5624fe3cd307f6962209e12d7165c4c312a48444
asdfvar <asdfvar@gmail.com>
2020-08-29 16:34:15 -0700

    more stuff

---
+++ write_copy_timing_test/write_time.cpp
@@ -0,0 +1,67 @@
+#include <sys/time.h>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <unistd.h>
+
+#define N 10000000
+
+int main ()
+{
+
+   struct timeval start, end;
+
+   float *x = new float[N];
+   float *y = new float[N];
+   double time;
+
+   std::ofstream myfile;
+
+   gettimeofday (&start, nullptr);
+   myfile.open ("data", std::ios::binary);
+   gettimeofday (&end, nullptr);
+   time = (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / 1000000.0f;
+std::cout << __FILE__ << __LINE__ << ":time = " << time << std::endl;
+
+   sleep (5.0f);
+
+   gettimeofday (&start, nullptr);
+   for (int ind = 0; ind < N; ind++) x[ind] = (float)(rand () % 100) / 100.0f;
+   gettimeofday (&end, nullptr);
+   time = (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / 1000000.0f;
+std::cout << __FILE__ << __LINE__ << ":time = " << time << std::endl;
+
+   sleep (5.0f);
+
+   gettimeofday (&start, nullptr);
+   for (int ind = 0; ind < N; ind++) myfile << x[ind];
+   gettimeofday (&end, nullptr);
+   time = (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / 1000000.0f;
+std::cout << __FILE__ << __LINE__ << ":time = " << time << std::endl;
+
+   sleep (5.0f);
+
+   gettimeofday (&start, nullptr);
+   myfile.close ();
+   gettimeofday (&end, nullptr);
+   time = (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / 1000000.0f;
+std::cout << __FILE__ << __LINE__ << ":time = " << time << std::endl;
+
+   sleep (5.0f);
+   gettimeofday (&start, nullptr);
+   for (int ind = 0; ind < N; ind++) y[ind] = x[ind];
+   gettimeofday (&end, nullptr);
+   time = (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / 1000000.0f;
+std::cout << __FILE__ << __LINE__ << ":time = " << time << std::endl;
+
+   gettimeofday (&start, nullptr);
+   delete[] x;
+   delete[] y;
+   gettimeofday (&end, nullptr);
+   time = (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / 1000000.0f;
+std::cout << __FILE__ << __LINE__ << ":time = " << time << std::endl;
+
+   sleep (5.0f);
+
+   return 0;
+}

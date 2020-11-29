3c7ab20699352cd81eaa5cdfdb5a75ea7c8daa3c
asdfvar <asdfvar@gmail.com>
2020-11-28 16:17:09 -0800

    .

--- tree/tree.py
+++ tree/tree.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python2
 
-import numpy as np
-
-class exampleContent:
-   pass
-
 class Node:
    def __init__ (self, depth, content, value):
       self.edge = True
@@ -19,36 +14,9 @@ class Node:
       self.edge = False
       return newNode
 
-   # Prune away all the nodes from this node that is not nearest to the target value
-   def prune (self, targetValue):
-      if self.edge: return self.value
-      values = []
-      for node in self.nodes:
-         values.append (node.prune (targetValue))
-      values = np.array (values)
-      minInd = np.argmin (np.abs (values - targetValue))
-      self.nodes = self.nodes[minInd]
-      self.value = self.nodes.value
-      return self.value
-
-   # Prune away all the nodes from this node that are not a path to the max target value
-   def maxPrune (self):
-      if self.edge: return self.value
-      maxValue = float ("-inf")
-      values = []
-      for node in self.nodes:
-         values.append (node.maxPrune ())
-      values = np.array (values)
-      ind = np.argmax (values)
-      self.nodes = [self.nodes[ind]]
-      self.value = self.nodes[0].value
-      return self.value
-
    def pruneKeepMin (self):
       for node in self.nodes:
          node.pruneKeepMin ()
-
-      if not self.edge: return
 
       minValue = float ("inf")
       minNode = None
@@ -59,6 +27,19 @@ class Node:
       if minNode is not None:
          self.nodes = [minNode]
 
+   def pruneKeepMax (self):
+      for node in self.nodes:
+         node.pruneKeepMin ()
+
+      maxValue = float ("-inf")
+      maxNode = None
+      for node in self.nodes:
+         if node.value > maxValue:
+            maxValue = node.value
+            maxNode = node
+      if maxNode is not None:
+         self.nodes = [maxNode]
+
    def __str__ (self):
       contents = self.depth * " " + str (self.value) + "\n"
       for node in self.nodes:
@@ -66,13 +47,24 @@ class Node:
       return contents
 
 # Example
-content = exampleContent
+if __name__ == "__main__":
+   class exampleContent: pass
+   content = exampleContent
 
-tree = Node (0, content, 0.0)
-node1 = tree.addNode (content, 1.0)
-node2 = node1.addNode (content, 2.0)
-node3 = node2.addNode (content, 3.0)
+   node0 = Node (0, content, 0.0)
+   node1 = node0.addNode (content, 1.0)
+   node2 = node1.addNode (content, 2.0)
+   node3 = node2.addNode (content, 3.0)
 
-node1.addNode (content, 2.1)
+   node0.addNode (content, 1.1)
+   node1.addNode (content, 2.1)
+   node3.addNode (content, 4.1)
+   node3.addNode (content, 4.2)
+   node3.addNode (content, 4.3)
 
-print (tree)
+   print ("before pruning:")
+   print (node0)
+
+   print ("after pruning:")
+   node0.pruneKeepMin ()
+   print (node0)
